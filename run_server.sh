0434a589fa08ec8c47ed1d04105979f8efad8c34
Pevansh <119294953+Pevansh@users.noreply.github.com>
2025-12-10 13:55:07 +0530

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /Users/pevanshgulati/Documents/100x_engineers/Newsletter_mcp
-exec /Users/pevanshgulati/Documents/100x_engineers/Newsletter_mcp/venv/bin/python -m src.server
+cd <path_of_the_folder>
+exec <path_of_the_folder>/venv/bin/python -m src.server
